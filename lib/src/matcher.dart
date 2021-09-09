215ecb1910b0d23cc5211309606291f447161cac
Ahmed Masoud <maxzod66@gmail.com>
2021-09-09 16:32:37 +0200

    comments

--- lib/src/matcher.dart
+++ lib/src/matcher.dart
@@ -1,12 +1,14 @@
-//* nonfunction-type-aliases
 import 'exceptions.dart';
 
+//* map blue print
 typedef MapBluePrint = Map<String, BluePrintFeild>;
 
+// * the matcher api for single feilds
 abstract class BluePrintFeild {
   void match(String key, Object? value);
 }
 
+// * for standard types
 class _BluePrintFeildT<T> implements BluePrintFeild {
   @override
   void match(String key, Object? value) {
@@ -24,24 +26,31 @@ class NumF extends _BluePrintFeildT<num> {}
 
 class BoolF extends _BluePrintFeildT<num> {}
 
+//* the Map handler
 class MapF extends BluePrintFeild {
   final MapBluePrint? mapBluePrint;
-
+  // * if you pass the bluePrint for thes Feild the matcher will matche aginst
+  //*  every single feild in the map other wise it will check if its just a Map
   MapF([this.mapBluePrint]);
 
   @override
   void match(String key, Object? value) {
+    //* inure is Map
     if (value is! Map) throw TypeDoesNotMatch(key: key, expected: Map);
     if (mapBluePrint != null) {
+      // * insure keys keys length
       if (mapBluePrint!.keys.length != value.keys.length) {
         throw LengthDoesNotMatch(expected: mapBluePrint!.length, key: key, vLength: value.length, value: value);
       }
+
+      /// * chick aginst keys values
       try {
         for (final childKey in mapBluePrint!.keys) {
           mapBluePrint![childKey]!.match('[$childKey]', value[childKey]);
         }
       } on TypeDoesNotMatch catch (e) {
         if (key.isEmpty) rethrow;
+        // * append the parent key if exist
         throw TypeDoesNotMatch(key: '$key' + e.key, value: e.value, expected: e.expected);
       }
     }
